32eb0b69e3b1b673d0c217d6d041d631b7759014
Jack Lawson <ajacksified@gmail.com>
2014-11-17 14:46:51 -0800

    Upgrade remarkable and enable sup

--- reddit.js
+++ reddit.js
@@ -1,8 +1,11 @@
 var Remarkable = require('remarkable');
+
 var converter = new Remarkable('commonmark', {
   linkify: true,
   typographer: true,
 });
+
+converter.inline.ruler.enable([ 'sup' ]);
 
 function process(text) {
   return converter.render(text);
