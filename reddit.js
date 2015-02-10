1b88249bb213acbc9bbaa18774d4c1d66a55d831
Jack Lawson <ajacksified@gmail.com>
2015-02-10 11:32:21 -0800

    HTML sanitization; v0.5.0

--- reddit.js
+++ reddit.js
@@ -1,10 +1,25 @@
 var snuownd = require('snuownd');
+var sanitize = require('sanitize-html');
+
 var processor = snuownd.getParser();
+
+var allowedTags = sanitize.defaults.allowedTags.concat(
+  [
+    'h1',
+    'h2',
+    'sup',
+  ]
+);
+
+var sanitizeOptions = {
+  allowedTags: allowedTags
+};
 
 function process(text) {
   if (!text) return text;
 
   text = processor.render(text);
+  text = sanitize(text, sanitizeOptions);
 
   return text;
 }
